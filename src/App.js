d9469575b955fa3d40fbecddd15f869c6f01ec8c
prabhupadswain <prabhupad.swain@outlook.com>
2021-10-25 23:00:35 +0530

    Axios package installed.App.js modifed to fetch data from Api and to check if it data is fetched successfully

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 import Header from './components/ui/Header';
 
-function App() {
+const App = () => {
+  const [launches, setLaunches] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const fetchLaunches = async () => {
+      const result = await axios('https://api.spacexdata.com/v3/launches');
+      //Check if data is coming from API or not. Printing data on the JS console 
+      //if data successfully fetched from API.
+      console.info(result.data) 
+    };
+
+    fetchLaunches();
+
+  }, []);
+
   return (
     <div className="App">
       <Header />
