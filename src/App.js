8b556f52c3e7eb49292aac36168f6878db00161b
prabhupadswain <prabhupad.swain@outlook.com>
2021-10-25 19:54:26 +0530

    App.js modified to add Header component.Header.js component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import Header from './components/ui/Header';
+
 function App() {
   return (
-    <div>
-      Hello World
+    <div className="App">
+      <Header />
     </div>
   );
 }
