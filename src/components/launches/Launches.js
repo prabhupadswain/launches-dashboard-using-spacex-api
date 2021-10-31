bf208bf71671fd1f3609f89159aef4bcfc235236
prabhupadswain <prabhupad.swain@outlook.com>
2021-10-31 12:02:01 +0530

    Application is desgined to ahve loading state and empty state indicators

--- src/components/launches/Launches.js
+++ src/components/launches/Launches.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import LaunchItem from './LaunchItem';
+import LaunchesList from './LaunchesList';
 import LaunchesFilter from './LaunchesFilter';
 import Spinner from '../ui/Spinner';
 
@@ -24,9 +24,9 @@ const Launches = ({ launches, loading }) => {
   }
 
   //Check if loading successful or not.
- if (loading) {
-  return <Spinner />; //Implementing the loading spinner
-}
+  if (loading) {
+    return <Spinner />; //Implementing the loading spinner
+  }
 
   //Display data in table format
   return (
@@ -35,34 +35,7 @@ const Launches = ({ launches, loading }) => {
         selected={filteredLaunchStatus}
         onChangeFilter={filterChangeHandler}
       />
-      <table className='table table-bordered table-hover'>
-        <thead>
-          <tr>
-            <th>No :</th>
-            <th>Launched(UTC)</th>
-            <th>Location</th>
-            <th>Mission</th>
-            <th>Orbit</th>
-            <th>Launch Status</th>
-            <th>Rocket</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredLaunches.map((launch, index) => (
-            <LaunchItem
-              key={index}
-              flightNumber={launch.flight_number}
-              launchDate={launch.launch_date_utc}
-              locationName={launch.launch_site.site_name}
-              missionName={launch.mission_name}
-              orbitName={launch.rocket.second_stage.payloads[0].orbit}
-              rocketName={launch.rocket.rocket_name}
-              upcomingFlag={launch.upcoming}
-              launchFlag={launch.launch_success}
-            />
-          ))}
-        </tbody>
-      </table>
+      <LaunchesList items={filteredLaunches} />
     </div>
   );
 };
