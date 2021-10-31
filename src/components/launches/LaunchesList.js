f8b923abc2b00c546ad8fa79050f0ddc6b3412fa
prabhupadswain <prabhupad.swain@outlook.com>
2021-10-31 19:55:09 +0530

    Previously developed components were modified to bring the modal feature in

--- src/components/launches/LaunchesList.js
+++ src/components/launches/LaunchesList.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 import LaunchItem from './LaunchItem';
 import './LaunchesList.css';
+import Modal from '../modal/Modal';
 
 const LaunchesList = (props) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [singleLaunchRecord, setSingleLaunchRecord] = useState([]);
+
+  //Bringing data from child component
+  function displayLaunchDataHandler(launch) {
+    setModalIsOpen(true);
+    setSingleLaunchRecord(launch);
+  }
+
+  //Close Modal
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   //Showing empty state indicator
   if (props.items.length === 0)
     return (
@@ -15,34 +30,41 @@ const LaunchesList = (props) => {
 
   //Showing data in table format
   return (
-    <table className='table table-bordered table-hover'>
-      <thead>
-        <tr>
-          <th>No :</th>
-          <th>Launched(UTC)</th>
-          <th>Location</th>
-          <th>Mission</th>
-          <th>Orbit</th>
-          <th>Launch Status</th>
-          <th>Rocket</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.items.map((launch, index) => (
-          <LaunchItem
-            key={index}
-            flightNumber={launch.flight_number}
-            launchDate={launch.launch_date_utc}
-            locationName={launch.launch_site.site_name}
-            missionName={launch.mission_name}
-            orbitName={launch.rocket.second_stage.payloads[0].orbit}
-            rocketName={launch.rocket.rocket_name}
-            upcomingFlag={launch.upcoming}
-            launchFlag={launch.launch_success}
-          />
-        ))}
-      </tbody>
-    </table>
+    <Fragment>
+      <table className='table table-bordered table-hover'>
+        <thead>
+          <tr>
+            <th>No :</th>
+            <th>Launched(UTC)</th>
+            <th>Location</th>
+            <th>Mission</th>
+            <th>Orbit</th>
+            <th>Launch Status</th>
+            <th>Rocket</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.items.map((launch, index) => (
+            <LaunchItem
+              key={index}
+              flightNumber={launch.flight_number}
+              launchDate={launch.launch_date_utc}
+              locationName={launch.launch_site.site_name}
+              missionName={launch.mission_name}
+              orbitName={launch.rocket.second_stage.payloads[0].orbit}
+              rocketName={launch.rocket.rocket_name}
+              upcomingFlag={launch.upcoming}
+              launchFlag={launch.launch_success}
+              launchRecord={launch}
+              onDisplayData={displayLaunchDataHandler}
+            />
+          ))}
+        </tbody>
+      </table>
+      {modalIsOpen && (
+        <Modal onClose={closeModalHandler} launchRecord={singleLaunchRecord} />
+      )}
+    </Fragment>
   );
 };
 
